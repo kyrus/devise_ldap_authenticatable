a432add683c0c7c47ffc5da9e5f59acf8a1e8ee0
crothe <crothe@gmail.com>
2014-02-27 09:48:17 -0800

    Update model.rb

--- lib/devise_ldap_authenticatable/model.rb
+++ lib/devise_ldap_authenticatable/model.rb
@@ -28,13 +28,13 @@ module Devise
         Devise::LDAP::Adapter.update_own_password(login_with, @password, current_password)
       end
       
-      def reset_password!(new_password, new_password_confirmation)
-        if new_password == new_password_confirmation && ::Devise.ldap_update_password
-          Devise::LDAP::Adapter.update_password(login_with, new_password)
-        end
-        clear_reset_password_token if valid?
-        save
-      end
+      #def reset_password!(new_password, new_password_confirmation)
+      #  if new_password == new_password_confirmation && ::Devise.ldap_update_password
+      #    Devise::LDAP::Adapter.update_password(login_with, new_password)
+      #  end
+      #  clear_reset_password_token if valid?
+      #  save
+      #end
 
       def password=(new_password)
         @password = new_password
